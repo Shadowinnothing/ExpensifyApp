5a6c46176cdcaa6c802d2738b1e5cd7f4b52eb92
Shadowinnothing <shadowinnothing@gmail.com>
2018-10-10 20:50:30 -0600

    Added 404 page using Switch from react-router-dom. Used a new package, react-youtube, to play HEYYEYAEAYEAY when its loaded cause its my project and Ill do what I want

--- src/app.js
+++ src/app.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Used for browser Routing
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+// Used for 404 page, plays youtube video
+import YouTube from 'react-youtube';
 
 // normalize is used to break default values down so app
 // looks the same in all browsers
@@ -33,14 +36,31 @@ const HelpPage = () => (
     </div>
 );
 
+const _404Opts = {
+    height: '390', 
+    width: '640',
+    playerVars: {
+        autoplay: 1
+    }
+}
+const NotFoundPage = () => (
+    <div>
+        <YouTube 
+            videoId="ZZ5LpwO-An4"
+            opts={_404Opts}
+        />
+    </div>
+);
+
 const routes = (
     <BrowserRouter>
-        <div>
+        <Switch>
             <Route path="/" component={ExpenseDashboardPage} exact={true}/> {/* exact route so / doesnt get called to every render */}
             <Route path="/create" component={AddExpensePage} />
             <Route path="/edit" component={EditExpensePage} />
             <Route path="/help" component={HelpPage} />
-        </div>
+            <Route component={NotFoundPage} />
+        </Switch>
     </BrowserRouter>
 );
 
