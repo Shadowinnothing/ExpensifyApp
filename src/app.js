07a4c68bf2947a3de79927c62bd0ab4f9aebf0a7
Shadowinnothing <shadowinnothing@gmail.com>
2018-10-10 19:54:55 -0600

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// normalize is used to break default values down so app
+// looks the same in all browsers
+import 'normalize.css/normalize.css';
+import './styles/styles.scss'; // <- custom scss files
+
+ReactDOM.render(<p>BoilerPlate</p>, document.getElementById('app'));
