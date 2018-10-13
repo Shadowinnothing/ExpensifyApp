90fb3be443bd615b5389b13d47a57e19a683e6c0
Shadowinnothing <shadowinnothing@gmail.com>
2018-10-13 17:10:13 -0600

    Playing around with redux a bit in the /playground/ folder

--- src/playground/redux101.js
+++ src/playground/redux101.js
@@ -1,9 +1,43 @@
 import {createStore} from 'redux';
 
 // similar to this.setState((prevState) => {});
-const store = createStore((state = {count: 0}) => {
-    
-    return state;
+const store = createStore((state = {count: 0}, action) => {
+
+    switch(action.type){
+        case 'INCREMENT':
+            return {
+                count: state.count + 1
+            };
+        case 'DECREMENT':
+            return {
+                count: state.count - 1
+            };
+        case 'RESET':
+            return {
+                count: state.count = 0
+            };
+        default:
+            return state;
+    }
 });
+
+const incrementCount = () => {
+    // dispatch sends off action to state
+    store.dispatch({
+        type: 'INCREMENT'
+    });
+};
+
+const decrementCount = () => {
+    store.dispatch({
+        type: 'DECREMENT'
+    });
+};
+
+const resetCount = () => {
+    store.dispatch({
+        type: 'RESET'
+    });
+};
 
 console.log(store.getState());
