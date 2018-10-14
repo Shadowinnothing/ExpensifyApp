faa8a165b94be9f31da31c7b52bc4a4687a9b2c1
Shadowinnothing <shadowinnothing@gmail.com>
2018-10-14 12:12:54 -0600

    Playing with redux more in the playground/ folder

--- src/playground/redux101.js
+++ src/playground/redux101.js
@@ -5,32 +5,52 @@ const store = createStore((state = {count: 0}, action) => {
 
     switch(action.type){
         case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
             return {
-                count: state.count + 1
+                count: state.count + incrementBy
             };
         case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
             return {
-                count: state.count - 1
+                count: state.count - decrementBy
             };
         case 'RESET':
             return {
                 count: state.count = 0
+            };
+        case 'SET':
+            return {
+                count: action.set
             };
         default:
             return state;
     }
 });
 
-const incrementCount = () => {
+// gets called EVERY time the store gets called
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+// unsibscribe from changes
+// NOTICE: will not work with this example because
+// I have placed the dispatch calls in their own functions
+//const unsubscribe = store.subscribe(() => {
+//    console.log('unsubscribe');
+//});
+
+const incrementCount = (x) => {
     // dispatch sends off action to state
     store.dispatch({
-        type: 'INCREMENT'
+        type: 'INCREMENT',
+        incrementBy: x
     });
 };
 
-const decrementCount = () => {
+const decrementCount = (x) => {
     store.dispatch({
-        type: 'DECREMENT'
+        type: 'DECREMENT',
+        decrementBy: x
     });
 };
 
@@ -40,4 +60,15 @@ const resetCount = () => {
     });
 };
 
-console.log(store.getState());
+const setCount = (x) => {
+    store.dispatch({
+        type: 'SET',
+        set: x
+    })
+}
+
+incrementCount();
+incrementCount(5);
+decrementCount(10);
+resetCount();
+setCount(69);
