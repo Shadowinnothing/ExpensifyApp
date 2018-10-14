5ddf7dfcb30a88b1d7c63124cb3fd5a75fa4838b
Shadowinnothing <shadowinnothing@gmail.com>
2018-10-14 15:21:13 -0600

    Added spread operators to reducers in playground/ folder. I have a feeling reduxExpensify.js will be added into the actual project later

--- src/playground/reduxExpensify.js
+++ src/playground/reduxExpensify.js
@@ -1,7 +1,31 @@
 import {createStore, combineReducers} from 'redux';
+import uuid from 'uuid';
 
 // ADD_EXPENSE
+const addExpense = (
+    {
+        description = '',
+        note = '',
+        amount = 0,
+        createdAt = 0
+    } = {}
+) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    }
+});
+
 // REMOVE_EXPENSE
+const removeExpense = ({id} = {}) => ({
+    type: 'REMOVE_EXPENSE',
+    id
+});
+
 // EDIT_EXPENSE
 // SET_TEXT_FILTER
 // SORT_BY_DATE
@@ -13,6 +37,13 @@ import {createStore, combineReducers} from 'redux';
 const expensesReducerDefaultState = [];
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
     switch(action.type){
+        case 'ADD_EXPENSE':
+            return [
+                ...state,
+                action.expense
+            ];
+        case 'REMOVE_EXPENSE':
+            return state.filter(({id}) => id !== action.id);
         default:
             return state;
     };
@@ -40,20 +71,26 @@ const store = createStore(
     })
 );
 
-console.log(store.getState());
+store.subscribe(() => {
+    console.log(store.getState());
+});
 
-const demoState = {
-    expenses: [{
-        id: 'lkjhgfdsa',
-        description: 'October Rent',
-        note: 'Final payment for address',
-        amount: 54500,
-        createdAt: 0
-    }],
-    filters: {
-        text: 'rent',
-        sortBy: 'amount', //date or amount
-        startDate: undefined,
-        endDate: undefined
-    }
-};
+const expenseOne = store.dispatch(addExpense({description: 'rent', amount: 100}));
+const expenseTwo = store.dispatch(addExpense({description: 'cofefe', amount: 350}));
+store.dispatch(removeExpense({id: expenseTwo.expense.id}));
+
+// const demoState = {
+//     expenses: [{
+//         id: 'lkjhgfdsa',
+//         description: 'October Rent',
+//         note: 'Final payment for address',
+//         amount: 54500,
+//         createdAt: 0
+//     }],
+//     filters: {
+//         text: 'rent',
+//         sortBy: 'amount', //date or amount
+//         startDate: undefined,
+//         endDate: undefined
+//     }
+// };
