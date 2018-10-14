95585b079631ede41e7651416dcb451f586e2e0c
Shadowinnothing <shadowinnothing@gmail.com>
2018-10-14 13:00:42 -0600

    Playing with ES6 Array Destructoring in the playground/ folder

--- src/playground/destructoring.js
+++ src/playground/destructoring.js
@@ -1,3 +1,10 @@
+/**
+ * 
+ * Object Destructoring
+ * 
+ */
+
+
 // const person = {
 //     name: 'Ryan',
 //     age: 22,
@@ -15,13 +22,31 @@
 //     console.log(`It's ${stateTemperature} in ${state}`);
 // }
 
-const book = {
-    title: 'American Psycho',
-    author: 'Bret Easton Eliis',
-    publisher: {
-        name: 'Vintage Books'
-    }
-}
+// const book = {
+//     title: 'American Psycho',
+//     author: 'Bret Easton Eliis',
+//     publisher: {
+//         name: 'Vintage Books'
+//     }
+// }
 
-const {name: publisherName = 'Self-Published'} = book.publisher;
-console.log(publisherName);
+// const {name: publisherName = 'Self-Published'} = book.publisher;
+// console.log(publisherName);
+
+
+/**
+ * 
+ * Array Destructoring
+ * 
+ */
+
+const address = ['12141 Bannock St', 'Denver', 'Colorado', '80234'];
+//const address = [];
+
+const [, city, state = "Utah"] = address;
+
+console.log(`You are in ${city}, ${state}`);
+
+const item = ['coffee (hot)', '$2.00', '$2.50', '$2.75'];
+const [coffee, , mediumCost] = item;
+console.log(`A medium ${coffee} costs ${mediumCost}`);
