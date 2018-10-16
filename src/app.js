b85879a52c9c2cd2d2d38e1d5bf111e014cf7250
Shadowinnothing <shadowinnothing@gmail.com>
2018-10-15 23:10:16 -0600

    Sorted the /playground/reduxExpenses into multiple files in the src/ directory. Also includes one example in app.js of how to import and use the store in the app. Next step is merging the redux with the react app

--- src/app.js
+++ src/app.js
@@ -1,9 +1,34 @@
+// react stuff
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// components
 import AppRouter from './routers/AppRouter';
 
+// store
+import configureStore from './store/configureStore';
+import {addExpense, removeExpense, editExpense} from './actions/expenses';
+import {setTextFilter} from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+
+// CSS
 import 'normalize.css/normalize.css'; // <- normalize is used to break default values down so app looks the same in all browsers
 import './styles/styles.scss'; // <- custom scss files
+
+const store = configureStore();
+store.dispatch(addExpense({
+  description: 'Water Bill',
+  amount: 120.43,
+  createdAt: -11
+}));
+store.dispatch(addExpense({
+  description: 'Gas Bill',
+  amount: 84.67,
+  createdAt: 10
+}));
+
+store.dispatch(setTextFilter('bill'));
+const state = store.getState();
+console.log(getVisibleExpenses(state.expenses, state.filters));
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
