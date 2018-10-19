bd99462a8fd9eeb857b02e1564935dd270699f89
Shadowinnothing <shadowinnothing@gmail.com>
2018-10-19 14:43:21 -0600

    Set up Date Picker in ExpenseForm to make the createdAt value in the store a lot easier to work with than traditional createdAt methods

--- src/componenets/ExpenseForm.js
+++ src/componenets/ExpenseForm.js
@@ -1,11 +1,20 @@
 import React, {Component} from 'react';
+import moment from 'moment';
+import {SingleDatePicker} from 'react-dates';
+
+import 'react-dates/lib/css/_datepicker.css'; // <- makes react-dates look sexy as hell
+
+const now = moment();
+console.log(now.format('MMM Do, YYYY'));
 
 export default class ExpenseForm extends Component {
 
   state = {
     description: '',
     note: '',
-    amount: ''
+    amount: '',
+    createdAt: moment(),
+    calanderFocused: false
   };
 
   onDescriptionChange = (e) => {
@@ -21,9 +30,17 @@ export default class ExpenseForm extends Component {
 
   onAmountChange = (e) => {
     const amount = e.target.value;
-    if(amount.match( /^\d*(\.\d{0,2})?$/ )){
+    if(amount.match(/^\d*(\.\d{0,2})?$/)){
       this.setState(() => ({amount}));
     }
+  };
+
+  onDateChange = (createdAt) => {
+    this.setState(() => ({createdAt}));
+  };
+
+  onFocusChange = ({focused}) => {
+    this.setState(() => ({calanderFocused: focused}));
   };
 
   render(){
@@ -38,17 +55,29 @@ export default class ExpenseForm extends Component {
             value={this.state.description}
             onChange={this.onDescriptionChange}
           />
+
           <input
             type="text"
             placeholder="Amount"
             value={this.state.amount}
             onChange={this.onAmountChange}
           />
+
+          <SingleDatePicker
+            date={this.state.createdAt}
+            onDateChange={this.onDateChange}
+            focused={this.state.calanderFocused}
+            onFocusChange={this.onFocusChange}
+            numberOfMonths={1}
+            isOutsideRange={(day) => false}
+          />
+
           <textarea
             placeholder="Add a note for your expense (optional)"
             value={this.state.note}
             onChange={this.onNoteChange}
           ></textarea>
+
         <button>Add Expense</button>
         </form>
 
