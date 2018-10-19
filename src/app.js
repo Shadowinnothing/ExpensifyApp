fec2683c2f82dc1fb6adec25ff9235cee214f8b8
Shadowinnothing <shadowinnothing@gmail.com>
2018-10-19 11:15:07 -0600

    Added component ExpenseListFilters.js which has a text input field to filter the expenses. also added a remove button in ExpenseListItem.js to remove individual expenses

--- src/app.js
+++ src/app.js
@@ -28,14 +28,14 @@ store.dispatch(addExpense({
   createdAt: 108453
 }));
 
-store.dispatch(setTextFilter('water'));
+store.dispatch(setTextFilter('bill'));
 
-setTimeout(() => {
-  store.dispatch(setTextFilter('bill'));
-}, 3000);
+// setTimeout(() => {
+//   store.dispatch(setTextFilter('bill'));
+// }, 3000);
 
 const state = store.getState();
-console.log(getVisibleExpenses(state.expenses, state.filters));
+//console.log(getVisibleExpenses(state.expenses, state.filters));
 
 const jsx = (
   <Provider store={store}>
--- src/componenets/ExpenseListItem.js
+++ src/componenets/ExpenseListItem.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
-const ExpenseListItem = ({description, amount, createdAt}) => (
+import {removeExpense} from '../actions/expenses';
+
+const ExpenseListItem = ({dispatch, description, amount, createdAt, id}) => (
   <div>
     <h3>Description: {description}</h3>
     <p>${amount} - {createdAt}</p>
     <br />
+    <button
+      onClick={() => {
+        dispatch(removeExpense({id}))
+      }}
+    >Remove</button>
   </div>
 );
 
-export default ExpenseListItem;
+export default connect()(ExpenseListItem);
