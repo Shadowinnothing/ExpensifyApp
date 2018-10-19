c2c1ae6d19f6377a9ca0270795b348a7a41cdc5e
Shadowinnothing <shadowinnothing@gmail.com>
2018-10-19 14:02:21 -0600

    Created the ExpenseForm.js component to allow users to input a new Expense

--- src/componenets/AddExpensePage.js
+++ src/componenets/AddExpensePage.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import ExpenseForm from './ExpenseForm';
+
 const AddExpensePage = () => (
     <div>
-        <p>all component yo</p>
+        <h1>Add Expense</h1>
+        <ExpenseForm />
     </div>
 );
 
