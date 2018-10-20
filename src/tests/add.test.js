dfc1e5f0a2f98f026616a495c3a87fbd8f22bfb3
Shadowinnothing <shadowinnothing@gmail.com>
2018-10-20 14:57:37 -0600

    Added jest with some super simple examples. kids stuff

---
+++ src/tests/add.test.js
@@ -0,0 +1,19 @@
+const add = (a, b) => a + b
+
+const generateGreeting = (name = 'Anon') => `Hello ${name}!`;
+
+it('should add 2 nums', () => {
+  const num = add(12, 12);
+
+  expect(num).toBe(24);
+});
+
+it('should say hello', () => {
+  const greeting = generateGreeting('Ryan');
+  expect(greeting).toBe('Hello Ryan!');
+});
+
+it('should say hi to anon', () => {
+  const greeting = generateGreeting();
+  expect(greeting).toBe('Hello Anon!');
+});
