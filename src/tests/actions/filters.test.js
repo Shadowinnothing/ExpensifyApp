737eeb8c2bb3719009e140cee8c092b969c863e5
Shadowinnothing <shadowinnothing@gmail.com>
2018-10-20 16:03:17 -0600

    Added test cases for actions/filters.js. Still pretty straightforward stuff

---
+++ src/tests/actions/filters.test.js
@@ -0,0 +1,35 @@
+import moment from 'moment';
+
+import {setTextFilter, sortByDate, sortByAmount,
+  setStartDate, setEndDate} from '../../actions/filters';
+
+describe('actions from actions/filters.js', () => {
+
+  test('should generate setTextFilter', () => {
+    const text = 'rent';
+    expect(setTextFilter(text)).toEqual({type: 'SET_TEXT_FILTER', text});
+  });
+
+  test('should generate default setTextFilter', () => {
+    expect(setTextFilter()).toEqual({type: 'SET_TEXT_FILTER', text: ''});
+  });
+
+  test('should sort by the amount', () => {
+    expect(sortByAmount()).toEqual({type: 'SORT_BY_AMOUNT'});
+  });
+
+  test('should sort by the date', () => {
+    expect(sortByDate()).toEqual({type: 'SORT_BY_DATE'});
+  });
+
+  test('should generate setStartDate', () => {
+    const startDate = moment(0);
+    expect(setStartDate(startDate)).toEqual({type: 'SET_START_DATE', startDate});
+  });
+
+  test('should generate setEndDate', () => {
+    const endDate = moment(12);
+    expect(setEndDate(endDate)).toEqual({type: 'SET_END_DATE', endDate});
+  });
+
+});
