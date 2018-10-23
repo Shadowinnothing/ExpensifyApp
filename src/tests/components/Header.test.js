35ee02344c857007632c670db80c90bc1e9dc391
Shadowinnothing <shadowinnothing@gmail.com>
2018-10-22 21:17:30 -0600

    Setup Enzyme to make testing with snapshots a lot more efficient

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+//import ReactShallowRenderer from 'react-test-renderer/shallow';
+import {shallow} from 'enzyme';
 
 import Header from '../../components/Header';
 
 describe('components/Header.js', () => {
 
   test('should render Header correctly', () => {
-    const renderer = new ReactShallowRenderer();
-    renderer.render(<Header />);
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
-    //console.log(renderer.getRenderOutput());
+    // const renderer = new ReactShallowRenderer();
+    // renderer.render(<Header />);
+    // expect(renderer.getRenderOutput()).toMatchSnapshot();
+    const wrapper = shallow(<Header />);
+    expect(wrapper).toMatchSnapshot();
+    //expect(wrapper.find('h1').text()).toBe('Expensify'); // <- enzyme example, dope yo
   });
 
 });
---
+++ src/tests/setupTests.js
@@ -0,0 +1,8 @@
+// This file Sets Up Enzyme for react-16
+
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({
+  adapter: new Adapter()
+});
