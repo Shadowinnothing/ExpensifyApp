b0a655fa98ad259abbbfc87697d7a3b598e982b2
Shadowinnothing <shadowinnothing@gmail.com>
2018-10-27 21:42:53 -0600

    Finished ExpenseListFilters tests. This is the finale of Section 12: Testing your application

--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+import moment from 'moment';
 
 import {ExpenseListFilters} from '../../components/ExpenseListFilters';
 import {filters, altFilters} from '../fixtures/filters';
@@ -35,6 +36,50 @@ describe('components/ExpenseListFilters', () => {
       filters: altFilters
     });
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test('should handle text change', () => {
+    const value = 'rent';
+    wrapper.find('input').simulate('change', {
+      target: {value}
+    });
+    expect(setTextFilterSpy).toHaveBeenLastCalledWith(value);
+  });
+
+  test('should sort by date', () => {
+    const value = 'date';
+    // by default, the tests already sort by date
+    // this call to setProps changes it to sort by amount
+    // to double check sort by date can be sorted to
+    wrapper.setProps({
+      filters: altFilters
+    });
+    wrapper.find('select').simulate('change', {
+      target: {value}
+    });
+    expect(sortByDateSpy).toHaveBeenCalled();
+  });
+
+  test('should sort by amount', () => {
+    const value = 'amount';
+    wrapper.find('select').simulate('change', {
+      target: {value}
+    });
+    expect(sortByAmountSpy).toHaveBeenCalled();
+  });
+
+  test('should handle date changes', () => {
+    const startDate = moment(0).add(4, 'years');
+    const endDate = moment(0).add(8, 'years');
+    wrapper.find('DateRangePicker').prop('onDatesChange')({startDate, endDate});
+    expect(setStartDateSpy).toHaveBeenLastCalledWith(startDate);
+    expect(setEndDateSpy).toHaveBeenLastCalledWith(endDate);
+  });
+
+  test('should handle date focus changes', () => {
+    const calanderFocused = 'endDate';
+    wrapper.find('DateRangePicker').prop('onFocusChange')(calanderFocused);
+    expect(wrapper.state('calanderFocused')).toBe(calanderFocused);
   });
 
 });
