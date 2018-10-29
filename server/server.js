c6b4591bff14ecaa0f810596f5b166777c082566
Shadowinnothing <shadowinnothing@gmail.com>
2018-10-28 21:10:32 -0600

    Setup production build and server. Added Heroku-cli to deploy the production build to heroku

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3030;
 const publicPath = path.join(__dirname, '..', 'public');
 
 app.use(express.static(publicPath));
@@ -12,6 +11,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App is running on Port: ${PORT}`);
 });
