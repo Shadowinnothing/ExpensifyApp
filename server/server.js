952717e13da702278fb797e8966fed6b08a6b202
Shadowinnothing <shadowinnothing@gmail.com>
2018-10-28 19:55:27 -0600

    Added some Node, which ive been DYING to do with React, and got my own server working. Added the webpack compiled files to .gitignore because im not 100% sure i need them on github

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3030;
+const publicPath = path.join(__dirname, '..', 'public');
+
+app.use(express.static(publicPath));
+
+// makes sure that visiting a url directly doesnt throw a 404
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`App is running on Port: ${PORT}`);
+});
