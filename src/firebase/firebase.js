e0e0b77ec5d9ccebcd629c3edebc1334755633d1
Shadowinnothing <shadowinnothing@gmail.com>
2018-11-01 21:03:19 -0600

    Learned how to remove data from firebase with remove() and set()

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -7,27 +7,42 @@ const database = firebase.database();
 
 // ref() -> reference, Different parts of the database
 // users, expenses, etc.
-database.ref().set({
-  name: 'Ryan',
-  age: 23,
-  isSingle: false,
-  location: {
-    city: 'Denver',
-    country: 'United States'
-  }
-}).then(() => {
-  console.log('Data has been saved');
-}).catch((err) => {
-  console.log('Failure: ', err);
-});
+database.ref()
+  .set({
+    name: 'Ryan',
+    age: 23,
+    isSingle: false,
+    location: {
+      city: 'Denver',
+      country: 'United States'
+    }
+  })
+  .then(() => {
+    console.log('Data has been saved');
+  })
+  .catch((err) => {
+    console.log('Failure: ', err);
+  });
+// database.ref('isSingle')
+//   .remove()
+//   .then(() => {
+//     console.log('successfully removed the data');
+//   })
+//   .catch((err) => {
+//     console.log('Failure: ', err);
+//   });
+database.ref('isSingle').set(null);
 
+//
+// just a few examples
+//
 // database.ref('age').set(24);
 // database.ref('location/city').set('Green Bay');
-database.ref('attributes').set({
-  height: 70,
-  weight: 180
-}).then(() => {
-  console.log('second set call worked yo');
-}).catch((err) => {
-  console.log('ERROR ERROR', err)
-});
+// database.ref('attributes').set({
+//   height: 70,
+//   weight: 180
+// }).then(() => {
+//   console.log('second set call worked yo');
+// }).catch((err) => {
+//   console.log('ERROR ERROR', err)
+// });
