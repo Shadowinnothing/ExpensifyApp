29c6e2dcbc0628c235e638f73423b47d79499607
Shadowinnothing <shadowinnothing@gmail.com>
2018-11-01 21:17:10 -0600

    Learned about update() calls to firebase that can set multiple values in the DB at once

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -11,7 +11,11 @@ database.ref()
   .set({
     name: 'Ryan',
     age: 23,
-    isSingle: false,
+    stressLevel: 4,
+    job: {
+      title: 'Software Developer',
+      company: 'Google'
+    },
     location: {
       city: 'Denver',
       country: 'United States'
@@ -23,6 +27,33 @@ database.ref()
   .catch((err) => {
     console.log('Failure: ', err);
   });
+
+database.ref()
+  .update({
+    stressLevel: 9,
+    'job/company': 'Amazon',
+    'location/city': 'Boulder'
+  })
+  .then(() => console.log('worked'))
+  .catch((err) => console.log(err));
+
+// database.ref()
+//   .update({
+//     name: 'Gel',
+//     age: 20,
+//     job: 'Office Manager',
+//     isSingle: null
+//   })
+//   .then(() => {
+//     console.log('works');
+//   })
+//   .catch((err) => {
+//     console.log('doesnt work');
+//   });
+
+//
+// both of these examples remove data
+//
 // database.ref('isSingle')
 //   .remove()
 //   .then(() => {
@@ -31,10 +62,10 @@ database.ref()
 //   .catch((err) => {
 //     console.log('Failure: ', err);
 //   });
-database.ref('isSingle').set(null);
+// database.ref('isSingle').set(null);
 
 //
-// just a few examples
+// just a few examples for manipulating data
 //
 // database.ref('age').set(24);
 // database.ref('location/city').set('Green Bay');
