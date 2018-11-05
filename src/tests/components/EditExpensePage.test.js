dee742f1407f1d7e83f8e579c4a3f657a6bfa2f1
Shadowinnothing <shadowinnothing@gmail.com>
2018-11-05 11:35:45 -0700

    Added functionality to remove an expense from firebase

--- src/tests/components/EditExpensePage.test.js
+++ src/tests/components/EditExpensePage.test.js
@@ -6,16 +6,16 @@ import expenses from '../fixtures/expenses';
 
 describe('components/EditExpensePage', () => {
 
-  let wrapper, editExpenseSpy, removeExpenseSpy, historySpy;
+  let wrapper, editExpenseSpy, startRemoveExpenseSpy, historySpy;
 
   beforeEach(() => {
     editExpenseSpy = jest.fn();
-    removeExpenseSpy = jest.fn();
+    startRemoveExpenseSpy = jest.fn();
     historySpy = {push: jest.fn()}
     wrapper = shallow(<EditExpensePage
       expense={expenses[0]}
       editExpense={editExpenseSpy}
-      removeExpense={removeExpenseSpy}
+      startRemoveExpense={startRemoveExpenseSpy}
       history={historySpy}
     />);
 
@@ -31,10 +31,10 @@ describe('components/EditExpensePage', () => {
     expect(editExpenseSpy).toHaveBeenLastCalledWith(expenses[0].id, expenses[0]);
   });
 
-  test('should handle removeExpenseSpy', () => {
+  test('should handle startRemoveExpenseSpy', () => {
     wrapper.find('button').simulate('click');
     expect(historySpy.push).toHaveBeenLastCalledWith('/');
-    expect(removeExpenseSpy).toHaveBeenLastCalledWith({id: expenses[0].id});
+    expect(startRemoveExpenseSpy).toHaveBeenLastCalledWith({id: expenses[0].id});
   });
 
 });
