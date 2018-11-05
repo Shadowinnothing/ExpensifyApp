5bdc7dda59e52d3822af7900f086cf08dcaeee6f
Shadowinnothing <shadowinnothing@gmail.com>
2018-11-05 16:28:56 -0700

    Added private routes to keep users that arent logged in away from certain parts of the app

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,7 +4,6 @@ import {Router, Route, Switch} from 'react-router-dom'; // Used for browser Rout
 import createHistory from 'history/createBrowserHistory';
 
 // Components
-import Header from '../components/Header';
 import HelpPage from '../components/HelpPage';
 import LoginPage from '../components/LoginPage';
 import NotFoundPage from '../components/NotFoundPage';
@@ -12,17 +11,19 @@ import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 
+// Routers
+import PrivateRoute from './PrivateRoute';
+
 export const history = createHistory();
 
 const AppRouter = () => (
     <Router history={history}>
         <div>
-            <Header />
             <Switch>
                 <Route path="/" component={LoginPage} exact={true} /> {/* exact route so / doesnt get called to every render */}
-                <Route path="/dashboard" component={ExpenseDashboardPage} />
-                <Route path="/create" component={AddExpensePage} />
-                <Route path="/edit/:id" component={EditExpensePage} />
+                <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
+                <PrivateRoute path="/create" component={AddExpensePage} />
+                <PrivateRoute path="/edit/:id" component={EditExpensePage} />
                 <Route path="/help" component={HelpPage} />
                 <Route component={NotFoundPage} /> // 404
             </Switch>
