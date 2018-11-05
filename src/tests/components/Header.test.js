eaaf6dafb570f785959c3e28fd505d0e835c9554
Shadowinnothing <shadowinnothing@gmail.com>
2018-11-05 15:15:31 -0700

    Added logout button to the header and updated some test cases for logging in and logging out

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 //import ReactShallowRenderer from 'react-test-renderer/shallow';
 import {shallow} from 'enzyme';
 
-import Header from '../../components/Header';
+import {Header} from '../../components/Header';
 
 describe('components/Header.js', () => {
 
@@ -10,9 +10,16 @@ describe('components/Header.js', () => {
     // const renderer = new ReactShallowRenderer();
     // renderer.render(<Header />);
     // expect(renderer.getRenderOutput()).toMatchSnapshot();
-    const wrapper = shallow(<Header />);
+    const wrapper = shallow(<Header startLogout={() => {} }/>);
     expect(wrapper).toMatchSnapshot();
     //expect(wrapper.find('h1').text()).toBe('Expensify'); // <- enzyme example, dope yo
+  });
+
+  test('should call startLogout on button click', () => {
+    const startLogoutSpy = jest.fn(); // spy
+    const wrapper = shallow(<Header startLogout={startLogoutSpy} />);
+    wrapper.find('button').simulate('click');
+    expect(startLogoutSpy).toHaveBeenCalled();
   });
 
 });
--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -10,4 +10,11 @@ describe('components/LoginPage', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
+  test('should call startLogin on button click', () => {
+    const startLoginSpy = jest.fn(); // spy
+    const wrapper = shallow(<LoginPage startLogin={startLoginSpy} />);
+    wrapper.find('button').simulate('click');
+    expect(startLoginSpy).toHaveBeenCalled();
+  });
+
 });
