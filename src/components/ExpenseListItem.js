a4221975ad5be8915715797ea5c3f46a170a3e99
Shadowinnothing <shadowinnothing@gmail.com>
2018-11-08 18:10:33 -0700

    Styled the expenses quite a bit for both mobile and desktop

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -4,20 +4,16 @@ import moment from 'moment';
 import numeral from 'numeral';
 
 const ExpenseListItem = ({description, amount, createdAt, id}) => (
-  <div>
-
-    <Link to={`/edit/${id}`}>
+  <Link className="list-item" to={`/edit/${id}`}>
+    <div>
       <h3>{description}</h3>
-    </Link>
+      <span>{moment(createdAt).format('MMMM Do, YYYY')}</span>
+    </div>
 
-    <p>
-      {numeral(amount / 100).format('$0,0.00')}
-      -
-      {moment(createdAt).format('MMMM Do, YYYY')}
-    </p>
-    <br />
-
-  </div>
+    <div>
+      <h3>{numeral(amount / 100).format('$0,0.00')}</h3>
+    </div>
+  </Link>
 );
 
 export default ExpenseListItem;
